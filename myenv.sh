10df53357b71847034b27ebed9cd998789e86479
Danilo Medeiros <dmedeiros@pagerduty.com>
2025-08-05 17:18:08 +0100

    Remove old nvim config

--- myenv.sh
+++ myenv.sh
@@ -2,6 +2,7 @@ codeview() {
     z $1 || return 1
     tmux split-window -h
     tmux resize-pane -R 70
+    tmux send-keys -t 1 'git status' Enter
     tmux select-pane -L
     tmux split-window -v
     tmux resize-pane -D 20
